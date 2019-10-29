9ee7ab180171ce12a6cc090f87a11d1912f08171
afp <afp@anl.gov>
2019-10-29 12:24:34 -0500

    Corrected the Mask and offset for reading link status.
    Comment out write to the Control register because it causes bus errors.

--- evgMrmApp/src/fct.cpp
+++ evgMrmApp/src/fct.cpp
@@ -36,9 +36,10 @@ FCT::~FCT() {}
 epicsUInt16 FCT::statusRaw() const
 {
     epicsUInt32 cur = READ32(base, Status);
-    cur &= 0xff;
-    WRITE32(base, Control, cur); // clear VIO latches
-    return ~cur; // invert to get 1==Ok
+    cur &= 0xff0000;
+//    WRITE32(base, Control, cur1); // clear VIO latches
+//    return ~cur; // invert to get 1==Ok
+    return (cur >> 16);
 }
 
 double FCT::dcUpstream() const
